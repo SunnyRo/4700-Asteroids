bb154942f18f6813fea4d4a4afa6a1d5db8a9d18
DarkSpark0905 <41309144+DarkSpark0905@users.noreply.github.com>
2020-11-23 10:57:46 -0800

    Spawn/Collision
    Random Spawning, Bullet collosion, Score

--- Variant/Objects/SeekingEnemy.gd
+++ Variant/Objects/SeekingEnemy.gd
@@ -4,7 +4,10 @@ var velocity = Vector2(0,0)
 var desired = Vector2()
 var theta
 var speed = 1
+var life = 10
 onready var player = get_tree().get_nodes_in_group("player")[0]
+onready var bullet_node = get_tree().get_root().find_node("ScoreNum",true,false)
+onready var bScore = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,3 +25,15 @@ func display():
 func seek():
 	velocity = Vector2(speed,0)
 	position += velocity.rotated(rotation)
+
+func _on_SeekingEnemy_body_entered(body):
+	if body.is_in_group("player"):
+		body.gethit()
+		queue_free()
+		
+func damage(amount: int):
+	life -= amount
+	if life <= 0:
+		bScore += 100
+		bullet_node.update_score(bScore)
+		queue_free()
